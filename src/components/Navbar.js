d8b12d63b29f4c666f6a91c69ad3bcdbfb99f873
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-07-28 12:41:11 +0300

    Create a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './styles/NavBar.css'
+
+
+
+
+
+const NavBar = () => {
+    
+ 
+  return (
+    <nav className='nav'>
+      
+      <ul>
+        <li>
+          <p>
+            About
+          </p>
+        </li>
+        <li>
+          <p>
+            Projects
+          </p>
+        </li>
+        <li>
+          <p>
+            Contact
+          </p>
+        </li>
+      </ul>
+    </nav>
+    
+  )
+}
+
+export default NavBar
