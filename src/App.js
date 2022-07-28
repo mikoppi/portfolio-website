9b1fd69bd20b5ea7d75f5db46221d6398ec6f7a7
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-07-28 19:41:46 +0300

    Work on hero animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import {React, useState} from 'react'
+import {React, useState, useRef,useEffect} from 'react'
 import About from './components/About'
 import Hero from './components/Hero'
 import NavBar from './components/Navbar'
 import Projects from './components/Projects'
 import './styles/Styles'
 
+
 const App = () => {
 
-  //const [coordinates, setCoordinates] = useState(null)
+  const [coordinates, setCoordinates] = useState(null)
+
+  const buttonRef = useRef()
+
+
+  //hanki elementin keskikohdan x ja y koordinaatit
+  //hanki hiiren koordinatit
+  //laske etäisyys hiiren koordinaatti - elementin keskikoordinaatti
+  //jaa etäisyydet keskikoordinaateilla --> käytä näitä tuloksia css tyylissä
 
   const getMouseCoord = (e) => {
     const xCoord = Math.round(
@@ -17,13 +26,23 @@ const App = () => {
       (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
     );
     const coords = { xCoord, yCoord };
-    //console.log(coords)
-    return coords
-  }   
-
+    console.log(coords)
+    setCoordinates(coords)
+    
+  }
+  
+  const onClickHandler = () =>
+    {
+        const position = buttonRef.current.getBoundingClientRect();
+        //This returns an object with left, top, right, bottom, x, y, width, and height.
+        console.log(position)
+    }
+  
+  
+  //console.log(coordinates)
   return (
     <div className='app'>
-      <Hero getMouseCoord={getMouseCoord}/>
+      <Hero getMouseCoord={getMouseCoord} coordinates={coordinates} buttonRef={buttonRef} onClickHandler={onClickHandler}/>
       <NavBar/>
       <About/>
       <Projects/>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import {React,useRef, useEffect} from 'react'
 
 
 
-const Hero = ({getMouseCoord}) => {
-  console.log(getMouseCoord)
+const Hero = ({getMouseCoord, coordinates, buttonRef, onClickHandler}) => {
+  
+  
+  //console.log(coordinates)
   const styles = { 
-    transform: `perspective(${1000}px) rotateX(${getMouseCoord.yCoord}deg) rotateY(${getMouseCoord.xCoord}deg) translateZ(${100}px)`,
+    transform: `perspective(${1000}px) rotateX(${0}deg) rotateY(${0}deg) translateZ(${100}px)`,
   };
+
+
+
   return (
     <section onMouseMove={(e) => getMouseCoord(e)} className='hero' >
-        <div className='hero-wrapper' style={styles}>
+        <div className='hero-wrapper' style={styles} >
           <h1 className='hero-title'>My name is Miko, I am a web developer</h1>
-          <button className='hero-button'>View my work</button>
+          <button className='hero-button' onClick={onClickHandler} ref={buttonRef}>View my work</button>
         </div>
     </section>
   )
