f38f57745a2e8772e582c5569e5e5e9156cacf38
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-01 11:15:41 +0300

    Connect contact form to EmailJS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        disableSendButton()
+        
+        const button = document.querySelector('.contact-form-button-text')
+        emailjs.sendForm(
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
+            form.current,
+            process.env.REACT_APP_USER_ID
+        )
+          .then((result) => {
+                button.textContent = 'Thank you for your message!'
+                console.log(result.text);
+          }, (error) => {
+                button.textContent =
+                'There was an error sending your message. Please refresh the page and try again.'
+                console.log(error.text);
+          });
+          form.current.reset()
+          
+      };
+    
+      const disableSendButton = () => {
+        const button = document.querySelector('.contact-form-button-text')
+    
+        button.disabled = true
+        button.style.cursor = 'default'
+        button.textContent = 'Sending...'
+        
+      }
+
   return (
     <section className='contact'>
         <div className='contact-wrapper'>
@@ -8,12 +45,12 @@ const Contact = () => {
             <p className='contact__paragraph'>
                 
             </p>
-            <form className='form'>
+            <form className='form' ref={form} onSubmit={sendEmail}>
                 <input
                 aria-label='Enter your name'
                 className='form-name'
                 type='text'
-                name='user_name'
+                name='name'
                 placeholder='Name'
                 required
                 />
@@ -21,7 +58,7 @@ const Contact = () => {
                 aria-label='Enter your email address'
                 className='form-email'
                 type='email'
-                name='user_email'
+                name='email'
                 placeholder='Email'
                 required
                 />
