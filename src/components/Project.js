a5e9415956887a84d8f52cdd7ce1a9b056b25baa
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-18 19:02:43 +0300

    Install react-simple-image-slider

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,36 +1,39 @@
-import React from 'react'
+import React from "react";
+import SimpleImageSlider from "react-simple-image-slider";
 
-
-const Project = ({title, description, tech, videoSource, url1,url2}) => {
+const Project = ({ title, description, tech, pictures, url1, url2 }) => {
   return (
-    <div className='project'>
-        <h3 className='project-title'>{title}</h3>
-        <p className='project-description'>{description}</p>
-        <div className='project-tech-wrapper'>
-            <p className='project-tech'>
-                <i>{tech}</i>
-            </p> 
+    <div className="project">
+      <h3 className="project-title">{title}</h3>
+      <p className="project-description">{description}</p>
+      <div className="project-tech-wrapper">
+        <p className="project-tech">
+          <i>{tech}</i>
+        </p>
+      </div>
+      <div className="project-buttons">
+        <div className="live-button">
+          <a href={url1}>
+            <button>Live demo</button>
+          </a>
         </div>
-        <div className='project-buttons'>
-            <div className="live-button">
-                <a href={url1}
-                ><button>Live demo</button></a
-                >
-            </div>
-            <div className="source-button">
-                <a href={url2}
-                ><button>View code</button></a
-                >
-            </div>
+        <div className="source-button">
+          <a href={url2}>
+            <button>View code</button>
+          </a>
         </div>
-        <div className="video-wrapper">
-              <video playsInline autoPlay loop>
-                <source src={videoSource} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
+        <div>
+          <SimpleImageSlider
+            width={896}
+            height={504}
+            images={pictures}
+            showBullets={true}
+            showNavs={true}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import Project from './Project'
 import { motion } from "framer-motion"
-import waldo from '../videos/whereswaldo.mp4'
-import pokemon from '../videos/Screencast 2022-08-14 15:27:09.mp4'
+import shopImage1 from '../styles/utils/images/Screenshot_2022-08-14_11-58-44.png'
+import shopImage2 from '../styles/utils/images/Screenshot_2022-08-14_11-59-53.png'
+
+
+const shopImages = [
+  {url: shopImage1},
+  {url: shopImage2},
+ 
+]
 
 const Projects = () => {
   return (
@@ -21,7 +28,7 @@ const Projects = () => {
             description='An e-commerce web application to display games, that are on discount on the actual 
             Steam game store. It allows users to add items to shopping cart, edit quantities and remove items.'
             tech='Built with React using the CheapShark API '
-            videoSource='https://github.com/mikoppi/portfolio-website/blob/gh-pages/static/media/onlineshop.34bc3bab29dd1fbed588.mp4'
+            pictures={shopImages}
             url1='https://mikoppi.github.io/online-shop/'
             url2='https://github.com/mikoppi/online-shop'
             />
@@ -38,7 +45,7 @@ const Projects = () => {
             description='Playstation 1 themed photo tagging app inspired by Wheres Waldo. 
             Find three characters as fast as possible and try to be the #1 on the global leaderboard!'
             tech='Built using React and Firebase'
-            videoSource={waldo}
+            pictures={shopImages}
             url1=''
             url2='https://github.com/mikoppi/wheres-waldo'
             />
@@ -54,7 +61,7 @@ const Projects = () => {
             title='PokeMemory'
             description='Pokemon themed memory card game. Score 24 to beat the game'
             tech='Built with React'
-            videoSource={pokemon}
+            pictures={shopImages}
             url1='https://mikoppi.github.io/memory-card-game/'
             url2='https://github.com/mikoppi/memory-card-game'
             />
